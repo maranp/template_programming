dd0745c2a62cfeb4a24e15a38530a09db331d449
Maran Pakkirisamy <pmaran.me@gmail.com>
2018-05-19 10:07:42 +0530

    sfinae friendly traits

--- traits/plus2.hpp
+++ traits/plus2.hpp
@@ -12,12 +12,30 @@
 //};
 
 // more resilient, sfinae friendly
+//template <typename T1, typename T2, typename = std::void_t<>>
+//struct plus_type : std::false_type {};
+//
+//template <typename T1, typename T2>
+//struct plus_type<T1, T2,
+//  std::void_t<decltype(declval<T1>() + declval<T2>())>>
+//  : std::true_type {
+//  using type = decltype(declval<T1>() + declval<T2>());
+//};
+
+// more clear and organised implementation
 template <typename T1, typename T2, typename = std::void_t<>>
-struct plus_type : std::false_type {};
+struct has_plus : std::false_type {};
 
 template <typename T1, typename T2>
-struct plus_type<T1, T2,
+struct has_plus<T1, T2,
   std::void_t<decltype(declval<T1>() + declval<T2>())>>
-  : std::true_type {
+  : std::true_type {};
+
+// plus_type has a type only if T1 + T2 is meaningful
+template <typename T1, typename T2, bool = has_plus<T1, T2>::value>
+struct plus_type {
   using type = decltype(declval<T1>() + declval<T2>());
 };
+
+template <typename T1, typename T2>
+struct plus_type<T1, T2, false> {};
