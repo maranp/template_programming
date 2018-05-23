0f6279df027e9f38258ea81b3eec619a66e95b2b
Maran Pakkirisamy <maran.pakkirisamy@cavium.com>
2018-05-23 16:44:07 +0530

    is_convertible

--- traits/is_convertible.cpp
+++ traits/is_convertible.cpp
@@ -7,14 +7,17 @@
 
 #include "../ihelper.h"
 
-
+template <typename From, typename To, bool = std::is_void_v<To> || std::is_array_v<To> || std::is_function_v<To>>
+struct is_convble_helper {
+  using type = std::integral_constant<bool, std::is_void_v<From> && std::is_void_v<To>>;
+};
 // To test whether a From type can be converted to To type
 // we rely on the language feature that,
 // if a From type is passed to a function and the function
 // takes To type, the function(To) will be selected
 // if From to To conversion is possible.
 template <typename From, typename To>
-struct is_convble_helper {
+struct is_convble_helper<From, To, false> {
 private:
   static void aux(To);
   template <typename Fr, typename = decltype(aux(declval<Fr>()))>
